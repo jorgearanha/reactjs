7bccd36f21c8397310ebd1f9da102becf1ea8cae
Jorge Luis Neves Aranha - JAA <jorge.aranha@iteris.com.br>
2020-03-10 13:56:58 -0300

    Aula 42

---
+++ react-complete-guide/src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+  state = {
+    persons: [
+      {
+        name: 'Jorge',
+        age: 25
+      },
+      {
+        name: 'Max',
+        age: 28
+      }
+    ]
+  }
+
+  switchNameHandler = () => {
+    console.log('Was clicked!');
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My Hobbies: ...</Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+      </div>
+    );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App'));
+  }
+}
+
+export default App;
