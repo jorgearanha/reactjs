87ef981ff7a537ad4b18e7102b3d416af7d4cf2d
Jorge Luis Neves Aranha - JAA <jorge.aranha@iteris.com.br>
2020-03-11 10:33:44 -0300

    Aula 49

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -47,19 +47,31 @@ class App extends Component {
   }
 
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button onClick={() => this.switchNameHandler('Omiranha')}>Switch Name</button>
-        <Person 
-          name={this.state.persons[0].name} 
+        <button
+          style={style}
+          onClick={() => this.switchNameHandler('Omiranha')}>
+          Switch Name
+          </button>
+        <Person
+          name={this.state.persons[0].name}
           age={this.state.persons[0].age}
-          click={this.switchNameHandler.bind(this, 'Miranha')}/>
-        <Person 
-          name={this.state.persons[1].name} 
-          age={this.state.persons[1].age} 
-          changed={this.nameChangedHandler}/>
+          click={this.switchNameHandler.bind(this, 'Miranha')} />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          changed={this.nameChangedHandler} />
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App'));
